7c6227cfa945dbd4425aecd5e794e8a949cc4666
Tarek <dr.tareqelmasry@hotmail.com>
2022-03-13 13:46:19 +0300

    configurations

--- app/lib/jwt_handler/jwt_handler.rb
+++ app/lib/jwt_handler/jwt_handler.rb
@@ -1,24 +1,14 @@
 
 module JwtHandler
 
-  class Coder
-    def initialize 
-
-
-    end
-  end
-
   JWT_SECRET_KEY =  Rails.application.credentials.dig(:jwt , :secret_key)
-  DEFAULT_EXPIRE_TIME=  1.hour.from_now.to_i
-  DEFUALT_ISSUER = "SD_API"
-  HEADERS=  {type: "JWT"}
 
   def self.encode (data={})
     payload = data[:payload] || {}
-    payload[:exp] = data[:expires_in]&.to_i || DEFAULT_EXPIRE_TIME
-    payload[:iss] = DEFUALT_ISSUER
+    payload[:exp] = data.dig(:expires_in)&.to_i
+    payload[:iss] = self.config.dig(:iss)
 
-    headers = HEADERS
+    headers = self.config.dig(:headers)
     if data[:headers].present?
       data[:headers].map {|k,v| headers[k] = v}
     end
@@ -30,6 +20,14 @@ module JwtHandler
     JWT.decode token , JWT_SECRET_KEY , true , { algorithm: 'HS256' }
   end
 
-  
+  def self.config
+    data = {}
+    config_file = File.join(Rails.root, 'config', 'jwt_handler.yml')
+    YAML.load(File.open(config_file)).each do |key, value|
+      data[key.to_s] = value
+    end if File.exists?(config_file)
+
+    data
+  end
 
 end
