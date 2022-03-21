8ec6f6f42d95cf4c1da7f7217b3a385e7c16e06f
Tarek <dr.tareqelmasry@hotmail.com>
2022-03-21 19:02:51 +0300

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,16 @@ class User < ApplicationRecord
   has_one :cart
   has_many :staff_actions
   
-  validates :email, uniqueness:true
-  validates :phone_no , uniqueness: true
+  validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+  validates :email,:phone_no, uniqueness:true
   validates :gender, inclusion: {in: ['male','female'], message: I18n.t('errors.validations.user.gender')}
-  validates :email , presence:true
-  validates :first_name , presence: true
-  validates :last_name, presence: true
-  validates :gender , presence: true
+  validates :first_name , length: { minimum: 1, maximum: 20}
+  validates :last_name, length: { minimum: 1, maximum: 20}
   validates :phone_no, numericality: { only_integer: true, message: I18n.t('errors.validations.user.invalid_phone_no') }
   validate :valid_phone_no
 
   def valid_phone_no
+    return unless phone_no
     unless phone_no.length == 9 && phone_no[0]== "5"
       errors.add(:phone_no, I18n.t('errors.validations.user.invalid_phone_no'))
     end
