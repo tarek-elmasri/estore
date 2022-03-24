cc7fba2d9f62fd4c400a79e7cd6d9e08abb55c1c
Tarek <dr.tareqelmasry@hotmail.com>
2022-03-24 18:38:17 +0300

    record staff actions for item categories

--- app/controllers/api/v1/dashboard/item_categories_controller.rb
+++ app/controllers/api/v1/dashboard/item_categories_controller.rb
@@ -5,6 +5,7 @@ class Api::v1::Dashboard::ItemCategoriesController < Api::V1::Dashboard::Base
     respond_forbidden unless Current.user.is_authorized_to_create_item_category?
     ic= ItemCategory.new(item_categories_params)
     if ic.save
+      Current.user.staff_actions.create(action: :create_item_category, model: :item_category, model_id: ic.id)
       respond({item_category: ic})
     else
       respond_unprocessable(ic.errors)
@@ -17,6 +18,7 @@ class Api::v1::Dashboard::ItemCategoriesController < Api::V1::Dashboard::Base
     ic= ItemCategory.find_by_id(params[:id])
     respond_not_found unless ic
     ic.destroy
+    Current.user.staff_actions.create(action: :delete_item_category, model: :item_category, model_id: ic.id)
     respond_ok
   end
 
