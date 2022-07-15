36bc5fc98124605cb1c7ecc4ee19278278a48ba1
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-07-15 20:12:39 +0300

    bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  constraints subdomain: "api" do
+  #constraints subdomain: "api" do
+  scope :api do
     scope module: "api" do
 
       scope module: "v1" do
