f51eb76f8dcb7f094c2a2d76967cf4ed323b0a36
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-07-15 17:15:21 +0300

    updates to test mailer function

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,16 +111,17 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.delivery_method = :smtp
-  config.default_url_options = {
-    host: 'https://sajastores.herokuapp.com'
-  }
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: 'smtp.gmail.com',,
+    domain: 'saja-stores.herokuapp.com',
     port: 587,
     user_name: Rails.application.credentials.dig(:gmail_account_settings, :username),
     password: Rails.application.credentials.dig(:gmail_account_settings, :password),
     authentication: 'plain',
     enable_starttls_auto: true
   }
+  config.default_url_options = {
+    host: 'saja-stores.herokuapp.com'
+  }
+  config.action_mailer.delivery_method = :smtp
 end
