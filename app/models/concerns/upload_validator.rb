4bb4ad3ccfc69db44e9b28822728b62efa426bc0
Tarek <dr.tareqelmasry@hotmail.com>
2022-07-18 00:15:18 +0300

    moving on

--- app/models/concerns/upload_validator.rb
+++ app/models/concerns/upload_validator.rb
@@ -4,21 +4,30 @@ module UploadValidator
   
   class_methods do
     def has_one_file(field_name, options={})
-      attr_accessor "base64_#{field_name}"
+      #attr_accessor "base64_#{field_name}"
       has_one_attached field_name
      
-      unless options.empty?
-        
-        define_method(:attachment_requirements) do
-          options
-        end
-
-        #private :attachment_requirements
-        
-        validate :attachment_requirements
+      define_method("base64_#{field_name}=") do |code|
+        file_containers ||= {}
+        file_containers["#{field_name}"] = {
+          decoder: Uploader::Decoder.new(code),
+          options: options
+        }
       end
 
+      define_method("base64_#{field_name}") do
+        file_containers["#{field_name}"].code
+      end
 
     end
+
+    validate :attachment_requirements
+  end
+
+  private
+  attr_accessor :file_containers
+
+  def attachment_requirements
+
   end
 end
