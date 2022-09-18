07b3de9c431e23d82aae824af9a9f581ab268022
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-09-18 13:39:58 +0300

    setting same site cookies config

--- config/initializers/cookie_store.rb
+++ config/initializers/cookie_store.rb
@@ -4,11 +4,14 @@ Rails.application.config.middleware.use ActionDispatch::Cookies
 Rails.application.config.middleware.use ActionDispatch::Session::CookieStore , 
     key: "sessions",
     secure: Rails.env=="production",
-    http_only: true
+    http_only: true,
+    same_site: :None
 
 Rails.application.config.middleware.insert_after ActionDispatch::Cookies , 
     ActionDispatch::Session::CookieStore,
     key: "sessions",
     secure: Rails.env=="production",
-    http_only: true
+    http_only: true,
+    same_site: :None
+
     
